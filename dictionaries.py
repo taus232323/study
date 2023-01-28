279e763919bef751aeb1e293e8154f686f7aa5cc
taus232323 <tayc13@list.ru>
2023-01-28 21:46:19 +0300

    update dicts

--- dictionaries.py
+++ dictionaries.py
@@ -1,2 +1,10 @@
-d = {"alice": 35, "mark": 28, "april": 19, "john": 30}
-print(d.get('a', -1))
+my_dict = [1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 2, 2, 2, 3, 3, 4, 5, 5, 6, 7, 7, 7, 7]
+count_dict = {}
+for element in my_dict:
+    if element in count_dict:
+        count_dict[element] += 1
+    else:
+        count_dict[element] = 1
+for key, value in count_dict.items():
+    print("Key: ", key, "value: ", value)
+
